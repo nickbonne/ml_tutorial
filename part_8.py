ba567e79ac2addac09ffca26ff1febdef60a3a14
Nick Bonne <nickbonne@gmail.com>
2017-08-22 22:29:20 -0600

    Following Sentdex's machine learning series. First commit.

---
+++ part_8.py
@@ -0,0 +1,79 @@
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+from statistics import mean
+from matplotlib.pyplot import style
+
+style.use('fivethirtyeight')
+
+# xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
+# ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+
+def create_dataset(hm, variance, step=2, correlation=False):
+
+    val = 1
+    ys = []
+
+    for i in range(hm):
+
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+
+        if correlation and correlation == 'pos':
+
+            val += step
+
+        elif correlation and correlation == 'neg':
+
+            val -= step
+
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
+
+
+def slope_int(xs, ys):
+
+    m = ( ( (mean(xs) * mean(ys)) - mean(xs * ys) ) /
+          ( (mean(xs) ** 2) - mean(xs ** 2) ) )
+
+    b = mean(ys) - (m * mean(xs))
+
+    return m, b
+
+
+def squared_error(ys_orig, ys_line):
+
+    return sum((ys_line - ys_orig) ** 2)
+
+
+def coef_determination(ys_orig, ys_line):
+
+    y_mean_line = [mean(ys_orig) for y in ys]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error( ys_orig, y_mean_line)
+
+    return 1 - (squared_error_regr / squared_error_y_mean)
+
+xs, ys = create_dataset(40, 10, 2, correlation='pos')
+
+m, b = slope_int(xs,ys)
+
+regress_line = [(m * x) + b for x in xs]
+
+predict_x = 8
+predict_y = (m * predict_x) + b
+
+r_squared = coef_determination(ys, regress_line)
+print(r_squared)
+
+plt.scatter(xs, ys)
+plt.plot(xs, regress_line)
+
+plt.scatter(predict_x, predict_y, color = 'g')
+plt.plot(xs, regress_line)
+
+plt.show()
+
